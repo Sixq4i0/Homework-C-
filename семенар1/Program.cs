4caf8d0aec0bff8db0ad917adf09dd508e68fb38
Sixq4i0 <kolsokov@vk.com>
2022-11-16 12:22:21 +0300

    add folders with dotnet

---
+++ семенар1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите числа");
+int a = int.Parse(Console.ReadLine()), b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine(a);  
+}
+if (a < b)
+{
+    Console.WriteLine(b);
+}
