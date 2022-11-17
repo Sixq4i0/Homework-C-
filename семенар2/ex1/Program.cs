df561c42a4934a0988a88bd60933438391b841f4
Sixq4i0 <kolsokov@vk.com>
2022-11-17 15:30:35 +0300

    firstex

--- семенар2/ex1/Program.cs
+++ семенар2/ex1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Введите трёхзначное число");
+string a =  Console.ReadLine(); 
+if (a.Length != 3 )
+{
+    Console.WriteLine("подумайте по лучше над числом которое ввели");
+}
+else 
+{
+    Console.WriteLine($"вторая цыфра введённого числа : {a[1]}");
+}
